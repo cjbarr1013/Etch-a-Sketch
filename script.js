e1b93c1e3e01f980f980e01fee6e5eb1f9bb1e20
Cameron Barr <cjbarr1013@gmail.com>
2024-05-05 22:39:44 -0400

    Add drawing constraints for better experience
    Must click before drawing, hovered square is highlighted.

--- script.js
+++ script.js
@@ -1,11 +1,20 @@
+/* Grid Drawing Event Listeners */
 const grid = document.querySelector(".grid");
-const gridDimensions = grid.getBoundingClientRect();
+let gridFlag = false;
+const gridFlagOff = () => {
+    gridFlag = false
+}
+const gridFlagOn = () => {
+    gridFlag = true
+}
+grid.addEventListener("mousedown", gridFlagOn)
+grid.addEventListener("mouseup", gridFlagOff)
+grid.addEventListener("mouseleave", gridFlagOff)
 
+/* Grid Slider */
 const slider = document.querySelector(".slider");
 const sliderOutput = document.querySelector(".slider-output");
 sliderOutput.textContent = slider.value;
-
-const resetBtn = document.querySelector("#reset-btn");
 
 slider.addEventListener("input", () => {
     sliderOutput.textContent = slider.value;
@@ -13,9 +22,17 @@ slider.addEventListener("input", () => {
 
 slider.addEventListener("mouseup", resetGrid)
 
+/* Reset Button */
+const resetBtn = document.querySelector("#reset-btn");
 resetBtn.addEventListener("click", resetGrid)
 
+/* Color Picker */
+const colorPicker = document.querySelector("#color-picker");
+/* colorPicker.addEventListener("input", ) */
+
+/* Functions */
 function createGrid(sliderValue) {
+    const gridDimensions = grid.getBoundingClientRect();
     const squareWidth = (gridDimensions.width / sliderValue) + "px";
     const squareHeight = (gridDimensions.height / sliderValue) + "px";
 
@@ -34,19 +51,31 @@ function createGrid(sliderValue) {
     }
 }
 
-function clearGrid() {
+function deleteGrid() {
     while (grid.firstChild) {
         grid.removeChild(grid.firstChild);
     }
 }
 
 function resetGrid() {
-    clearGrid();
+    deleteGrid();
     createGrid(slider.value);
 }
 
 function changeSquareColor() {
-    this.classList.add("grid-square-fill");
+    if (gridFlag) {
+        this.classList.add("grid-square-fill");
+    }
 }
 
 createGrid(slider.value);
+
+/* 
+To-do:
+- Add ability to change color
+- Make it so you have to "click" & "mouseover" to draw
+- Add rainbow button and functionality
+- Add darkening button and functionality
+- Add eraser button and functionality
+- Finish up styling
+ */
