ad0ccb67795ebb8317f2b0dd8828a68c53d6c49a
Cameron Barr <cjbarr1013@gmail.com>
2024-05-05 20:39:08 -0400

    Add grid size slider and reset button
    Also an input to change the color, but not yet functional.

--- script.js
+++ script.js
@@ -1,22 +1,47 @@
 const grid = document.querySelector(".grid");
-const gridDimensions = grid.getBoundingClientRect()
+const gridDimensions = grid.getBoundingClientRect();
 
-function createGrid() {
-    const squareWidth = (gridDimensions.width / 16) + "px"
-    const squareHeight = (gridDimensions.height / 16) + "px";
+const slider = document.querySelector(".slider");
+const sliderOutput = document.querySelector(".slider-output");
+sliderOutput.textContent = slider.value;
 
-    for (let i = 1; i <= (16**2); i++) {
+const resetBtn = document.querySelector("#reset-btn");
+
+slider.addEventListener("input", () => {
+    sliderOutput.textContent = slider.value;
+})
+
+slider.addEventListener("mouseup", resetGrid)
+
+resetBtn.addEventListener("click", resetGrid)
+
+function createGrid(sliderValue) {
+    const squareWidth = (gridDimensions.width / sliderValue) + "px";
+    const squareHeight = (gridDimensions.height / sliderValue) + "px";
+
+    for (let i = 1; i <= (sliderValue**2); i++) {
         const square = document.createElement("div");
-        square.classList.add("grid-square")
+        square.classList.add("grid-square");
         square.style.width = squareWidth;
         square.style.height = squareHeight;
-        square.addEventListener("mouseover", changeSquareColor)
+        square.addEventListener("mouseover", changeSquareColor);
         grid.appendChild(square);
     }
 }
 
-function changeSquareColor() {
-    this.classList.add("grid-square-hovered")
+function clearGrid() {
+    while (grid.firstChild) {
+        grid.removeChild(grid.firstChild);
+    }
 }
 
-createGrid()
+function resetGrid() {
+    clearGrid();
+    createGrid(slider.value);
+}
+
+function changeSquareColor() {
+    this.classList.add("grid-square-fill");
+}
+
+createGrid(slider.value);
